3081d9a3e592c4ac3a2f2aa88b274ce5c5daee3a
Rafael Almendra <rafaelalmendra28@gmail.com>
2022-02-06 13:58:08 -0300

    add changes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <Script
           strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=UA-219615209-1`}
+          src={`https://www.googletagmanager.com/gtag/js?id=UA-219615209-2`}
         />
 
         <Script id="tag" strategy="lazyOnload">
@@ -30,7 +30,7 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'UA-219615209-1', {
+            gtag('config', 'UA-219615209-2', {
               page_path: window.location.pathname,
             });
                 `}
